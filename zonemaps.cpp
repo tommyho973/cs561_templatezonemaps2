47f176758f0515c2fc669248ba76b89aec050b23
Tommy Ho <hot@scc-wl4.scc.bu.edu>
2025-01-23 19:51:11 -0500

    Added build and sort_elements

--- zonemaps.cpp
+++ zonemaps.cpp
@@ -2,9 +2,54 @@
 
 template<typename T>
 zonemap<T>::zonemap(std::vector<T> _elements, uint _num_elements_per_zone)
+    : elements(std:: move(_elements)), num_elements_per_zone(_num_elements_per_zone)
 {
     // constructor 
     // Your code starts here ...
+    if(num_elements_per_zone<1){
+        throw std::invalid_argument("Please enter a number greater than 0")
+    }
+    if(elements.empty()){
+        num_zones = 0;
+        return;
+    }
+    build();
+}
+
+template<typename T>
+void zonemap<T>::build(){
+    sort_elements();
+    num_zones = (total + num_elements_per_zone -1)/ num_elements_per_zone;
+    zone<T> current;
+    for(int i = 0; i < elements.size(); i++){
+        current.elements.push_back(elements[i]);
+        if(current.elements.size()==num_elements_per_zone || i == elements.size()-1){
+            current_zone.min = current_zone.elements.front();
+            current_zone.max = current_zone.elements.back();
+            current_zone.size = current_zone.elements.size();
+        }
+        zones.push_back(std::move(current_zone));
+        current_zone.elements.clear();
+
+    }
+}
+
+template<typename T>
+void zonemap<T>::sort_elements(){
+    bool swapped;
+    int n = elements.size();
+    for(int i = 0; i < n-1; i++){
+        swapped = false;
+        for(int j = 0; j < n-1; j++){
+            if(elements[j]> elements[j+1]){
+                std::swap(elements[j],elements[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped){
+            break;
+        }
+    }
 }
 
 template<typename T>
